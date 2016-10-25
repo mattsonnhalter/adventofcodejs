3ffd98e943a1188b6618ab58898a711049ab359f
Matt Sonnhalter <Matt Sonnhalter>
2016-10-25 01:12:58 -0500

    Working solution for single input

--- day02.js
+++ day02.js
@@ -1,16 +1,15 @@
 //read input from file
 var inputDimension = "4x23x21";
 
-
-
 //function to calculate the surface area of box and also determine the smallest surface area, returning the total ft 
-function CalculateSurfaceAreaAndSlack(element, index, array, inputDimension){
-    var inputDimensionArray = inputDimension.split("x");
+function CalculateSurfaceAreaAndSlack(input){
+    var inputDimensionArray = input.split("x");
     var length = inputDimensionArray[0];
     var width = inputDimensionArray[1];
     var height = inputDimensionArray[2];
-
-
+    result2 = CalculateSurfaceArea(length, width, height);
+    
+    console.log(result2);
 }
 
 
@@ -20,9 +19,11 @@ function CalculateSurfaceArea(length, width, height){
     var widthResult = 2*(width * height);
     var heightResult = 2*(length * width);
 
-    var result = lengthResult + widthResult + heightResult;
+    result = lengthResult + widthResult + heightResult;
     result = result + Math.min(lengthResult, widthResult, heightResult);
+    return result;
 }
 
 //iterate through function for each input and add to a total feet variable
-console.log(result);
+
+CalculateSurfaceAreaAndSlack(inputDimension);
